f42c2f0a41d326af1002491ede87716897a89281
Abdurrahman Gazi Yavuz <sniper.erratic.0w@icloud.com>
2024-08-17 12:21:40 -0700

    Page layout to display public/protected content

--- src/app/client/page.tsx
+++ src/app/client/page.tsx
@@ -1,5 +1,20 @@
+"use client";
+
+import PageLayout from "@/components/common/page-layout";
+
 export default function Client(){
     return(
-        <>Client</>
+        <PageLayout>
+            <PageLayout.Public>
+                <>Public Client</>
+                <>Appointment List</>
+                <>Nearby Service List</>
+            </PageLayout.Public>
+            <PageLayout.Protected>
+                <>Protected Client</>
+                <>Appointment List</>
+                <>Nearby Service List</>
+            </PageLayout.Protected>
+        </PageLayout>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
+"use client";
+
+import PageLayout from "@/components/common/page-layout";
+
 export default function Home() {
   return (
-      <>Dashboard</>
+    <PageLayout>
+      <PageLayout.Public>
+        <>Public Dashboard</>
+      </PageLayout.Public>
+      <PageLayout.Protected>
+        <>Protected Dashboard</>
+      </PageLayout.Protected>
+    </PageLayout>
   );
 }
