d16c5d8404b8e0d23aea73e30b57234d60b0b79a
Abdurrahman Gazi Yavuz <sniper.erratic.0w@icloud.com>
2024-08-17 15:47:12 -0700

    services mock

---
+++ src/components/store/new-service-dialog.tsx
@@ -0,0 +1,54 @@
+import {Plus} from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+
+export function NewServiceDialog() {
+    return (
+        <Dialog>
+            <DialogTrigger asChild>
+                <Button
+                    onClick={() => console.log('Add')}
+                    variant="default"
+                    size="sm"
+                    className="h-8 gap-1 text-sm"
+                >
+                    <span className="sr-only sm:not-sr-only">Add</span>
+                    <Plus className="h-8 w-8"/>
+                </Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-md">
+            <DialogHeader>
+                    <DialogTitle>Add Service</DialogTitle>
+                    <DialogDescription>
+                        Add a new service to your store
+                    </DialogDescription>
+                </DialogHeader>
+                <div className="flex items-center space-x-2">
+                    New Service Inputs here
+                </div>
+                <DialogFooter className="sm:justify-start">
+                    <DialogClose asChild>
+                        <Button type="button" variant="secondary">
+                            Add
+                        </Button>
+                    </DialogClose>
+                    <DialogClose asChild>
+                        <Button type="button" variant="secondary">
+                            Close
+                        </Button>
+                    </DialogClose>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    )
+}
--- src/components/store/service-card.tsx
+++ src/components/store/service-card.tsx
@@ -1,4 +1,8 @@
 import CustomCard from "@/components/common/custom-card";
+import {Button} from "@/components/ui/button";
+import {CircleMinus, EllipsisVertical} from "lucide-react";
+import {NewServiceDialog} from "@/components/store/new-service-dialog";
+import {useToast} from "@/components/ui/use-toast";
 
 interface SubCategory{
     id: number
@@ -32,20 +36,53 @@ interface Props{
 }
 
 const ServiceCard = (props: Props) => {
+    const { toast } = useToast()
     return(
         <CustomCard title=''>
+            <div className='flex items-start justify-start'>
+                <NewServiceDialog/>
+            </div>
             {props.data.services.map(service => (
                 <div key={service.id} className='mb-4'>
+                    <hr className="border-t border-gray-300 my-4"/>
+                    <div className='flex items-end justify-end'>
+                        <Button
+                            onClick={() => {
+                                toast({
+                                    title: "Uh oh! Something went wrong.",
+                                    description: "MockUp data cannot be deleted",
+                                })
+                            }}
+                            variant="default"
+                            size="sm"
+                            className="h-4 w-4 p-0 gap-1 text-sm"
+                        >
+                            <CircleMinus className="h-4 w-4"/>
+                        </Button>
+                        <Button
+                            onClick={() => {
+                                toast({
+                                    title: "Uh oh! Something went wrong.",
+                                    description: "MockUp data cannot be edited",
+                                })
+                            }}
+                            variant="default"
+                            size="sm"
+                            className="h-4 w-4 p-0 gap-1 text-sm"
+                        >
+                            <EllipsisVertical className="h-4 w-4"/>
+                        </Button>
+                    </div>
                     <p>{service.name}</p>
                     <p>{service.description}</p>
                     <p>{service.duration} min</p>
                     <p>${service.price}</p>
                     {service.categories.map(category => (
                         <div key={category.id}>
-                            <p>Category {category.name}</p>
+                            <p>{category.name}</p>
                             {category.sub_categories.map(sub_category => (
                                 <div key={sub_category.id}>
-                                    <p>SubCategory {sub_category.name}</p>
+                                    <p>{sub_category.name}</p>
                                 </div>
                             ))}
                         </div>
